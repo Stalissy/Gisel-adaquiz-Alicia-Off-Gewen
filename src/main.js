94023f21dc23177b1b4198671f3e18aec7509e16
Alicia <aliciap@magou.fr>
2026-03-10 14:29:32 +0100

    Question_suivante_debug

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import "./style.css";
 import { addQuestionHtml, btnValide } from "./readJson.js";
 
+// Charge un fichier
 fetch("/quiz.json")
+  // Trensforme le texte JSON en objet JS
   .then((response) => response.json())
+  // Dévini la variable data comme étant l'objet JS
   .then((data) => {
     addQuestionHtml(data.questions, 1, "app");
     btnValide(data.questions, 1, "valide");
--- src/readJson.js
+++ src/readJson.js
@@ -1,79 +1,92 @@
+// --- Fonctions d'extraction ---
 function extractQuestion(data, nbQuestion) {
-  const question = data[nbQuestion - 1].question;
-  return question;
+  return data[nbQuestion - 1].question;
 }
 
 function extractOptions(data, nbQuestion) {
-  const options = data[nbQuestion - 1].options;
-  return options;
+  return data[nbQuestion - 1].options;
 }
 
 function extractCorrectIndex(data, nbQuestion) {
-  const correctIndex = data[nbQuestion - 1].correctIndex;
-  return correctIndex;
+  return data[nbQuestion - 1].correctIndex;
 }
 
+// --- Affichage de la question ---
 export function addQuestionHtml(data, nbQuestion, divID) {
   const question = extractQuestion(data, nbQuestion);
   const options = extractOptions(data, nbQuestion);
 
   const div = document.getElementById(divID);
+
+  // Ajoute la question
   div.innerHTML = `<h3>${question}</h3>`;
 
-  let i = 0;
-  options.forEach((option) => {
+  // Ajoute les réponse possible
+  options.forEach((option, i) => {
     div.innerHTML += `<label><input type="checkbox" class="check-option">${option}</label><br>`;
-    i++;
   });
 
-  div.innerHTML += `<div id="nom"><button id="valide">Validée</button></div>`;
+  // Ajoute le bouton de validation
+  div.innerHTML += `
+    <div id="good-girl">
+      <button id="valide" class="btn">Validée</button>
+    </div>`;
 }
 
+// --- Gestion du bouton de validation ---
 export function btnValide(data, nbQuestion, btnID, divID) {
   const btn = document.getElementById(btnID);
 
   btn.addEventListener("click", () => {
     const checkboxes = document.querySelectorAll(".check-option");
     const checkOption = [];
-
+    // Enregiste les réponse soumise pour les comparée plus tard
     checkboxes.forEach((checkbox, index) => {
-      if (checkbox.checked) {
-        checkOption.push(index);
-      }
+      if (checkbox.checked) checkOption.push(index);
     });
+
+    // Vérifie si l'utilisateur à les bonne réponse
     comparReponse(data, nbQuestion, checkOption, divID);
   });
 }
 
+// --- Comparaison de la réponse ---
 function comparReponse(data, nbQuestion, checkOption, divID) {
   const correctIndex = extractCorrectIndex(data, nbQuestion);
+
   if (arraysEqual(correctIndex, checkOption)) {
-    goodGirl("nom");
+    goodGirl("good-girl");
   } else {
-    badGirl("nom");
+    badGirl("good-girl");
   }
+
   nextQuestion(data, nbQuestion, divID);
 }
 
+// --- Vérification d'égalité de tableaux ---
 function arraysEqual(a, b) {
   return a.length === b.length && a.every((val, i) => val === b[i]);
 }
 
-function goodGirl(divID) {
+// --- Affichage en cas de bonne réponse ---
+function goodGirl(divID, data, nbQuestion) {
   const div = document.getElementById(divID);
+  // Ajoute le texte disant si tu à réson et le bouton "question suvante"
   div.innerHTML = `
     <p>Bonne Fille</p>
     <button id="nextQuestion">Question Suivante</button>
   `;
 }
 
-function badGirl(divID) {
+function badGirl(divID, data, nbQuestion) {
   const div = document.getElementById(divID);
   div.innerHTML = `
     <p>Mauvaise Fille</p>
-    <button id="nextQuestion">Question Suivante</button>`;
+    <button id="nextQuestion">Question Suivante</button>
+  `;
 }
 
+// --- Passage à la question suivante ---
 function nextQuestion(data, nbQuestion, divID) {
   const btn = document.getElementById("nextQuestion");
 
