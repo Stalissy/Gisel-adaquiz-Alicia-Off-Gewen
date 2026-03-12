bcc6e6743d5e159d88f08136132c8b5fa98d93c5
Alicia <aliciap@magou.fr>
2026-03-12 11:06:08 +0100

    truc

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import "./style.css";
-import {
-  addQuestionHtml,
-  btnValide,
-  goodAnswers,
-  calcScore,
-  start,
-} from "./readJson.js";
+import { start } from "./readJson.js";
 
 const btnFemmeScientifique = document.getElementById("btn-femme-scientifique");
 const btnCultureTransfem = document.getElementById("btn-culture-transfem");
